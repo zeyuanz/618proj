f9cf11c6afd45fa6c254313d3fea5baed2eb17da
zeyuanz <zeyuanz@andrew.cmu.edu>
2021-04-14 21:31:48 -0400

    add non convex opt for SA

---
+++ opt_SA_serial.cpp
@@ -0,0 +1,18 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  opt_SA_serial.cpp
+ *
+ *    Description:  serial implementation of non-convex opt with SA
+ *
+ *        Version:  1.0
+ *        Created:  04/14/21 21:31:04
+ *       Compiler:  gcc
+ *
+ *         Author:  zeyuan zuo
+ *   Organization:  CMU
+ *
+ * =====================================================================================
+ */
+#include <stdlib.h>
+
